e9f781cd1935d9b71e4c4b41f400b6f63633bd03
Edenilson Junior <edenilsonju10@gmail.com>
2024-08-03 01:39:30 -0300

    feat: mapeamento das classes do pacote de entity/people

--- librigate/src/main/java/br/com/librigate/model/entity/people/Customer.java
+++ librigate/src/main/java/br/com/librigate/model/entity/people/Customer.java
@@ -2,21 +2,44 @@ package br.com.librigate.model.entity.people;
 
 import br.com.librigate.model.entity.actions.Buy;
 import br.com.librigate.model.entity.actions.Rent;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
+@DiscriminatorValue("customer")
 public class Customer extends Person {
 
+    @Column(name = "registration_date", nullable = false)
     private LocalDate registrationDate;
+
+    @Column(name = "active", nullable = false)
     private boolean active;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "purchases",
+            joinColumns = @JoinColumn(name = "customer_cpf"),
+            inverseJoinColumns = @JoinColumn(name = "buy_id")
+    )
     private List<Buy> purchases;
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "rents",
+            joinColumns = @JoinColumn(name = "customer_cpf"),
+            inverseJoinColumns = @JoinColumn(name = "rent_id")
+    )
     private List<Rent> rentList;
 
-    public Customer(String cpf, String firstName, String lastName, LocalDate birthDate, String gender,
-                    String telephone, String login, String password, Address address, LocalDate registrationDate,
-                    boolean active, List<Buy> purchases, List<Rent> rentList) {
+    public Customer() {}
+
+    public Customer(String cpf, String firstName, String lastName, LocalDate birthDate, String gender, String telephone, String login, String password, Address address, LocalDate registrationDate, boolean active, List<Buy> purchases, List<Rent> rentList) {
         super(cpf, firstName, lastName, birthDate, gender, telephone, login, password, address);
+
         this.registrationDate = registrationDate;
         this.active = active;
         this.purchases = purchases;
--- librigate/src/main/java/br/com/librigate/model/entity/people/Employee.java
+++ librigate/src/main/java/br/com/librigate/model/entity/people/Employee.java
@@ -1,20 +1,34 @@
 package br.com.librigate.model.entity.people;
 
 import br.com.librigate.model.entity.actions.Restock;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
+@DiscriminatorValue("customer")
 public class Employee extends Person {
 
     private String role;
+
+    @ManyToMany
+    @JoinTable(
+            name = "employee_restock",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "restock_id")
+    )
     private List<Restock> restockList;
+
+
+    public Employee(){}
 
     public Employee(String cpf, String firstName, String lastName, LocalDate birthDate, String gender, String telephone, String login, String password, Address address, String role, List<Restock> restockList) {
         super(cpf, firstName, lastName, birthDate, gender, telephone, login, password, address);
         this.role = role;
         this.restockList = restockList;
     }
+
 
     public String getRole() {
         return role;
