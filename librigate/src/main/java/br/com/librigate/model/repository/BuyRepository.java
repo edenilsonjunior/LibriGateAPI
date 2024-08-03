ebaaacda0fc98c7792617580654c345e02e17b6f
Edenilson Junior <edenilsonju10@gmail.com>
2024-08-03 01:04:35 -0300

    fix: adicao da annotation de repository nas classes de repository

--- librigate/src/main/java/br/com/librigate/model/repository/BuyRepository.java
+++ librigate/src/main/java/br/com/librigate/model/repository/BuyRepository.java
@@ -2,6 +2,8 @@ package br.com.librigate.model.repository;
 
 import br.com.librigate.model.entity.actions.Buy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BuyRepository extends JpaRepository<Buy, Long> {
 }
--- librigate/src/main/java/br/com/librigate/model/repository/FisicalBookRepository.java
+++ librigate/src/main/java/br/com/librigate/model/repository/FisicalBookRepository.java
@@ -2,6 +2,8 @@ package br.com.librigate.model.repository;
 
 import br.com.librigate.model.entity.book.FisicalBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FisicalBookRepository extends JpaRepository<FisicalBook, String> {
 }
