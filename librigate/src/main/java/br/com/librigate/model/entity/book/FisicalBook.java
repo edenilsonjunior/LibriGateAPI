6dd470b6c9249e8fca460dade06efae1cd49b86b
roberts <gabrielrobertssoares@gmail.com>
2024-08-03 01:42:52 -0300

    update annotations

--- librigate/src/main/java/br/com/librigate/model/entity/book/FisicalBook.java
+++ librigate/src/main/java/br/com/librigate/model/entity/book/FisicalBook.java
@@ -1,15 +1,34 @@
 package br.com.librigate.model.entity.book;
 
 import br.com.librigate.model.entity.actions.Review;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
+@Table(name = "fisical_book")
 public class FisicalBook extends Book {
 
+    @Id
+    @Column(name = "Copy")
     private int copyNumber;
+
+    @NotNull
+    @Column(name = "Status")
     private String status;
+
+    @NotNull
+    @Column(name = "Price")
     private double price;
+
+    public FisicalBook(){
+        super();
+    }
 
     public FisicalBook(String isbn, String title, String description, String publisher, String category, List<String> authorsName, int edition, LocalDate launchDate, List<Review> reviews, int copyNumber, String status, double price) {
         super(isbn, title, description, publisher, category, authorsName, edition, launchDate, reviews);
