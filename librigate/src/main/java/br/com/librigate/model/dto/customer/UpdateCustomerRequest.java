1b35b2bca6c4b748d6a493f157c0cdb04e965b99
roberts <gabrielrobertssoares@gmail.com>
2024-08-19 22:05:37 -0300

    dto atualização customer

--- librigate/src/main/java/br/com/librigate/model/dto/customer/UpdateCustomerRequest.java
+++ librigate/src/main/java/br/com/librigate/model/dto/customer/UpdateCustomerRequest.java
@@ -1,4 +1,16 @@
 package br.com.librigate.model.dto.customer;
 
-public record UpdateCustomerRequest() {
+import br.com.librigate.model.dto.AddressDTO;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public record UpdateCustomerRequest(
+        String cpf,
+        Optional<String> firstName,
+        Optional<String> lastName,
+        Optional<String> telephone,
+        Optional<String> password,
+        Optional<AddressDTO> address
+) {
 }
