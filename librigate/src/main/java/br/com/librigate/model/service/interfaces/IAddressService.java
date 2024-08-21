314dd0d22d6fbe70de00f70d1cd17d75242a083c
roberts <gabrielrobertssoares@gmail.com>
2024-08-20 22:48:47 -0300

    update nome adress dto

--- librigate/src/main/java/br/com/librigate/model/service/interfaces/IAddressService.java
+++ librigate/src/main/java/br/com/librigate/model/service/interfaces/IAddressService.java
@@ -1,14 +1,14 @@
 package br.com.librigate.model.service.interfaces;
 
 import br.com.librigate.exception.EntityNotFoundException;
-import br.com.librigate.model.dto.AddressDTO;
+import br.com.librigate.model.dto.AddressRequest;
 import br.com.librigate.model.entity.people.Address;
 
 public interface IAddressService {
 
-     public Address create(AddressDTO dto);
+     public Address create(AddressRequest dto);
 
      public Address findByPK(Long id) throws EntityNotFoundException;
 
-     public Address update(Long id, AddressDTO dto) throws EntityNotFoundException;
+     public Address update(Long id, AddressRequest dto) throws EntityNotFoundException;
 }
