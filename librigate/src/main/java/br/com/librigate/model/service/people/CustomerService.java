438b8545a13ecee88c924226f7b152c28c08896e
roberts <gabrielrobertssoares@gmail.com>
2024-08-20 22:44:13 -0300

    customer service

--- librigate/src/main/java/br/com/librigate/model/service/people/CustomerService.java
+++ librigate/src/main/java/br/com/librigate/model/service/people/CustomerService.java
@@ -4,91 +4,100 @@ import br.com.librigate.exception.EntityNotFoundException;
 import br.com.librigate.model.dto.customer.CreateCustomerRequest;
 import br.com.librigate.model.dto.customer.UpdateCustomerRequest;
 import br.com.librigate.model.dto.customer.review.ReviewRequest;
-import br.com.librigate.model.entity.actions.Review;
-import br.com.librigate.model.entity.people.Customer;
+import br.com.librigate.model.entity.people.Person;
 import br.com.librigate.model.mapper.interfaces.CustomerMapper;
 import br.com.librigate.model.repository.CustomerRepository;
-import br.com.librigate.model.service.book.BookService;
+import br.com.librigate.model.service.actions.ReviewService;
 import br.com.librigate.model.service.interfaces.ICustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.LocalDate;
 
+@Service
 public class CustomerService implements ICustomerService {
 
     private final CustomerRepository customerRepository;
     private final AddressService addressService;
-    private final BookService bookService;
+    private final ReviewService reviewService;
 
-    public CustomerService(CustomerRepository customerRepository, AddressService addressService, BookService bookService) {
+    public CustomerService( CustomerRepository customerRepository, AddressService addressService, ReviewService reviewService) {
         this.customerRepository = customerRepository;
         this.addressService = addressService;
-        this.bookService = bookService;
+        this.reviewService = reviewService;
     }
 
     @Override
-    public Customer create(CreateCustomerRequest request) {
+    public ResponseEntity<?> create(CreateCustomerRequest request) {
         try{
             var customer = CustomerMapper.instance.toEntity(request);
             var adress = addressService.create(request.address());
+            customer.setRegistrationDate(LocalDate.now());
             customer.setAddress(adress);
-            return customerRepository.save(customer);
+            return new ResponseEntity<>(customerRepository.save(customer), HttpStatus.CREATED);
         } catch (Exception e){
-            throw new RuntimeException("Error creating customer");
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @Override
-    public Customer update(UpdateCustomerRequest request) {
+    public ResponseEntity<?> update(UpdateCustomerRequest request) {
         try{
-             var entity = findByPK(request.cpf());
+             var entity = customerRepository
+                     .findById(request.cpf())
+                     .orElseThrow(()-> new EntityNotFoundException("Customer not found"));
+
              request.address().ifPresent((address) -> {
-                var updatedAddress = addressService.update(entity.getAddress().getId(), address);
-                entity.setAddress(updatedAddress);
+                var updatedAddress = addressService
+                        .update(entity.getAddress().getId(), address);
+                        entity.setAddress(updatedAddress);
              });
+
              request.firstName().ifPresent(entity::setFirstName);
              request.lastName().ifPresent(entity::setLastName);
              request.telephone().ifPresent(entity::setTelephone);
              request.password().ifPresent(entity::setPassword);
-             return entity;
+             return new ResponseEntity<>(customerRepository.save(entity), HttpStatus.CREATED);
         } catch (Exception e){
-            throw new RuntimeException("Error updating customer");
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @Override
-    public Customer findByPK(String id) throws EntityNotFoundException {
-        return customerRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Customer not found"));
+    public ResponseEntity<?> findByPK(String id) throws EntityNotFoundException {
+        var entity = customerRepository.findById(id);
+        if(entity.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(entity.get(), HttpStatus.OK);
     }
 
     @Override
-    public List<Customer> findAll() {
-        return customerRepository.findAll();
+    public ResponseEntity<?> findAll() {
+        return new ResponseEntity<>(customerRepository.findAll()
+                .stream().filter(Person::isActive), HttpStatus.OK);
     }
 
     @Override
-    public void delete(String id) {
-        var entity = findByPK(id);
+    public ResponseEntity<?> delete(String id) {
         try{
+            var entity = customerRepository.findById(id)
+                    .orElseThrow(()-> new EntityNotFoundException("Customer not found"));
             entity.setActive(false);
             customerRepository.save(entity);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e){
-            throw new RuntimeException("Error deleting customer");
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     @Override
-    public Review reviewBook(ReviewRequest request) {
-        try {
-            var entity = findByPK(request.cpf());
-            var book = bookService.findByPK(request.bookId());
-            var review = new Review();
-            review.setCustomer(entity);
-            review.setBook(book);
-            review.setDescription(request.description());
-            review.setRating(request.rating());
-            return review;
+    public ResponseEntity<?> reviewBook(ReviewRequest request) {
+        try{
+            return new ResponseEntity<>(reviewService.reviewBook(request), HttpStatus.CREATED);
         } catch (Exception e){
-            throw new RuntimeException("Error creating review");
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 }
