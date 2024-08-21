f64c01c2b532a473d2f4a585552fc5ecd7eee79e
roberts <gabrielrobertssoares@gmail.com>
2024-08-20 22:45:19 -0300

    interface customer service

--- librigate/src/main/java/br/com/librigate/model/service/interfaces/ICustomerService.java
+++ librigate/src/main/java/br/com/librigate/model/service/interfaces/ICustomerService.java
@@ -3,19 +3,13 @@ package br.com.librigate.model.service.interfaces;
 import br.com.librigate.exception.EntityNotFoundException;
 import br.com.librigate.model.dto.customer.CreateCustomerRequest;
 import br.com.librigate.model.dto.customer.UpdateCustomerRequest;
-import br.com.librigate.model.dto.customer.review.ReviewRequest;
-import br.com.librigate.model.entity.actions.Review;
-import br.com.librigate.model.entity.people.Customer;
-
-import java.util.List;
+import org.springframework.http.ResponseEntity;
 
 public interface ICustomerService  {
 
-    Customer create(CreateCustomerRequest request);
-    Customer update(UpdateCustomerRequest request);
-    Customer findByPK(String id) throws EntityNotFoundException;
-    List<Customer> findAll();
-    void delete(String id);
-
-    Review reviewBook(ReviewRequest request);
+    ResponseEntity<?> create(CreateCustomerRequest request);
+    ResponseEntity<?> update(UpdateCustomerRequest request);
+    ResponseEntity<?> findByPK(String id) throws EntityNotFoundException;
+    ResponseEntity<?> findAll();
+    ResponseEntity<?> delete(String id);
 }
