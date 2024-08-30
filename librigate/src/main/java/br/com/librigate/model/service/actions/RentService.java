630d1d4436ff735958e35236ca7ad6868b80b3f0
LeoVillaS <villasiqueiraleo23@gmail.com>
2024-08-30 15:50:47 -0300

    Implementa serviço de aluguel de livros (RentService) com validação e transações.

--- librigate/src/main/java/br/com/librigate/model/service/actions/RentService.java
+++ librigate/src/main/java/br/com/librigate/model/service/actions/RentService.java
@@ -1,16 +1,45 @@
 package br.com.librigate.model.service.actions;
 
-import java.util.List;
-
-import br.com.librigate.model.dto.customer.rent.RentRequest;
-import br.com.librigate.model.dto.customer.rent.RentResponse;
+import br.com.librigate.dto.actions.rent.RentRequest;
+import br.com.librigate.dto.actions.rent.RentResponse;
+import br.com.librigate.exception.EntityNotFoundException;
 import br.com.librigate.model.entity.actions.Rent;
+import br.com.librigate.model.entity.book.BookCopy;
+import br.com.librigate.model.entity.people.Customer;
+import br.com.librigate.model.repository.BookCopyRepository;
+import br.com.librigate.model.repository.CustomerRepository;
+import br.com.librigate.model.repository.RentRepository;
+import br.com.librigate.model.service.HandleRequest;
+import br.com.librigate.model.service.actions.factory.RentFactory;
+import br.com.librigate.model.service.actions.validator.RentValidator;
 import br.com.librigate.model.service.interfaces.IRentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @Service
-public class RentService implements IRentService{
+public class RentService implements IRentService {
+
+    private final RentRepository rentRepository;
+    private final CustomerRepository customerRepository;
+    private final BookCopyRepository bookCopyRepository;
+    private final RentFactory rentFactory;
+    private final RentValidator rentValidator;
+
+    @Autowired
+    public RentService(RentRepository rentRepository, CustomerRepository customerRepository,
+                       BookCopyRepository bookCopyRepository, RentFactory rentFactory, RentValidator rentValidator) {
+        this.rentRepository = rentRepository;
+        this.customerRepository = customerRepository;
+        this.bookCopyRepository = bookCopyRepository;
+        this.rentFactory = rentFactory;
+        this.rentValidator = rentValidator;
+    }
 
     @Transactional
     @Override
