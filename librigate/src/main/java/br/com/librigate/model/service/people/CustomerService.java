40bc7a4cd0490e3989dea30bdff532b70f839545
roberts <gabrielrobertssoares@gmail.com>
2024-08-31 23:26:24 -0300

    ajustes de padrão na classe service de customer

--- librigate/src/main/java/br/com/librigate/model/service/people/CustomerService.java
+++ librigate/src/main/java/br/com/librigate/model/service/people/CustomerService.java
@@ -3,103 +3,98 @@ package br.com.librigate.model.service.people;
 import br.com.librigate.exception.EntityNotFoundException;
 import br.com.librigate.dto.people.customer.CreateCustomerRequest;
 import br.com.librigate.dto.people.customer.UpdateCustomerRequest;
-import br.com.librigate.model.entity.people.Person;
-import br.com.librigate.model.mapper.people.CustomerMapper;
+import br.com.librigate.model.entity.people.Customer;
 import br.com.librigate.model.repository.CustomerRepository;
-import br.com.librigate.model.service.address.AddressService;
+import br.com.librigate.model.service.HandleRequest;
 import br.com.librigate.model.service.interfaces.ICustomerService;
+import br.com.librigate.model.service.people.factory.CustomerFactory;
+import br.com.librigate.model.service.people.validator.CustomerValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.security.InvalidParameterException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Service
 public class CustomerService implements ICustomerService {
 
     private final CustomerRepository customerRepository;
-    private final AddressService addressService;
+    private final CustomerValidator customerValidator;
+    private final CustomerFactory customerFactory;
 
-    public CustomerService(CustomerRepository customerRepository, AddressService addressService) {
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository, CustomerValidator customerValidator, CustomerFactory customerFactory) {
         this.customerRepository = customerRepository;
-        this.addressService = addressService;
+        this.customerValidator = customerValidator;
+        this.customerFactory = customerFactory;
     }
 
     @Transactional
     @Override
     public ResponseEntity<?> create(CreateCustomerRequest request) {
-        try{
-            var customer = CustomerMapper.instance.toEntity(request);
-            var address = addressService.create(request.address());
-            customer.setRegistrationDate(LocalDate.now());
-            customer.setAddress(address);
+        return HandleRequest.handle(() -> {
+            customerValidator.validateNewCustomer(request);
 
-            customer.setPurchases(new ArrayList<>());
-            customer.setRentList(new ArrayList<>());
+            var customer = customerFactory.createCustomer(request);
+            var response = customerRepository.save(customer);
 
-            return new ResponseEntity<>(customerRepository.save(customer), HttpStatus.CREATED);
-
-        } catch (InvalidParameterException ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-        } catch (Exception ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        });
     }
 
 
     @Transactional
     @Override
     public ResponseEntity<?> update(UpdateCustomerRequest request) {
-        try{
-             var entity = customerRepository
-                     .findById(request.cpf())
-                     .orElseThrow(()-> new EntityNotFoundException("Customer not found"));
+        return HandleRequest.handle(()->{
+            var customer = customerFactory.updateCustomer(request, findByCustomerByCPF(request.cpf()));
 
-             request.address().ifPresent((address) -> {
-                var updatedAddress = addressService
-                        .update(entity.getAddress().getId(), address);
-                        entity.setAddress(updatedAddress);
-             });
-
-             request.firstName().ifPresent(entity::setFirstName);
-             request.lastName().ifPresent(entity::setLastName);
-             request.telephone().ifPresent(entity::setTelephone);
-             request.password().ifPresent(entity::setPassword);
-             return new ResponseEntity<>(customerRepository.save(entity), HttpStatus.CREATED);
-        } catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+            var response = customerRepository.save(customer);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        });
     }
 
     @Override
-    public ResponseEntity<?> findByPK(String id) throws EntityNotFoundException {
-        var entity = customerRepository.findById(id);
-        if(entity.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(entity.get(), HttpStatus.OK);
+    public ResponseEntity<?> findByPK(String id){
+        return HandleRequest.handle(()->{
+            var response = customerRepository.findById(id);
+
+            if(response.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(response.get(), HttpStatus.OK);
+        });
     }
 
     @Override
     public ResponseEntity<?> findAll() {
-        return new ResponseEntity<>(customerRepository.findAll()
-                .stream().filter(Person::isActive), HttpStatus.OK);
+        return HandleRequest.handle(()->{
+            var response = customerRepository.findAll();
+
+            if(response.isEmpty()){
+                return new ResponseEntity<>(new ArrayList<Customer>(), HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        });
     }
 
     @Transactional
     @Override
-    public ResponseEntity<?> delete(String id) {
-        try{
-            var entity = customerRepository.findById(id)
-                    .orElseThrow(()-> new EntityNotFoundException("Customer not found"));
-            entity.setActive(false);
-            customerRepository.save(entity);
+    public ResponseEntity<?> delete(String cpf) {
+        return HandleRequest.handle(()->{
+            var customer = findByCustomerByCPF(cpf);
+            customer.setActive(false);
+
+            customerRepository.save(customer);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        });
+    }
+
+    private Customer findByCustomerByCPF(String cpf) throws EntityNotFoundException {
+        return customerRepository.findById(cpf)
+                .orElseThrow(()-> new EntityNotFoundException("Customer not found"));
     }
 }
